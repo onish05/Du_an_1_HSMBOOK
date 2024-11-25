ffde90daa7641408202afad6f931874027dc94ca
onish <152246656+onish05@users.noreply.github.com>
2024-11-25 08:38:06 +0700

    update: css

---
+++ view/boxright.php
@@ -0,0 +1,14 @@
+<div class="row mb">
+   <div class="boxtitle">TÀI KHOẢN</div>
+   <div class="boxcontent formtaikhoan">
+      <?php ?>
+   </div>
+</div>
+<div class="row mb">
+   <div class="boxtitle">DANH MỤC</div>
+   <div class="boxcontent2">
+      <div class="menudoc listdm">
+         <?php ?>
+      </div>
+   </div>
+</div>
