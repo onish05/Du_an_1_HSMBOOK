97b2c5f08e0248ae2628624c3e7c0b4f9b323164
onish05 <phamminh5610@gmail.com>
2024-11-29 08:12:04 +0700

    feat:sanpham

--- admin/header.php
+++ admin/header.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-<<<<<<< HEAD
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="../styles.css"> 
+    <link rel="stylesheet" href="../styles.css">
 </head>
+
 <body>
     <div class="boxcenter">
         <div class="row mb headeradmin">
@@ -26,37 +27,7 @@
                 <li><a href="index.php">Tài khoản</a></li>
                 <li><a href="index.php">Bình luận</a></li>
                 <li><a href="index.php">Thống kê</a></li>
-                <li><a href="../index.php">Trang website</a></li>                
+                <li><a href="../index.php">Trang website</a></li>
             </ul>
         </div>
     </div>
-
-=======
-
-<head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>ADMIN</title>
-   <link rel="stylesheet" href="css/style.css">
-</head>
-
-<body>
-   <div class="">
-      <div class="">
-         <h1>ADMIN</h1>
-      </div>
-      <div class="">
-         <ul>
-            <li><a href="">Trang chủ</a></li>
-            <li><a href="">Loại sách</a></li>
-            <li><a href="">Sản phẩm</a></li>
-            <li><a href="">Tài khoản</a></li>
-            <li><a href="">Bình luận</a></li>
-            <li><a href="">Trang public</a></li>
-         </ul>
-      </div>
-   </div>
-</body>
-
-</html>
->>>>>>> 5d9ac758d99a2205e5a98619bc4a4e7102cf218f
