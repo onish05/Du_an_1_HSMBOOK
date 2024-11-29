d138fd903a0efb09f436c18b363e1ef8d7736733
Huy Nguyen <vanhuynguyen2802@gmail.com>
2024-11-29 07:35:46 +0700

    feat:danhmuc

---
+++ admin/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="../styles.css"> 
+</head>
+<body>
+    <div class="boxcenter">
+        <div class="row mb headeradmin">
+            <h1>ADMIN</h1>
+        </div>
+        <div class="row mb menu">
+            <ul>
+                <li><a href="index.php">Trang chủ</a></li>
+                <li>
+                    <a href="javascript:void(0)">Danh mục <span class="arrow">▼</span></a>
+                    <ul class="submenu">
+                        <li><a href="index.php?act=adddm">Thêm danh mục</a></li>
+                        <li><a href="index.php?act=listdm">Danh sách danh mục</a></li>
+                    </ul>
+                </li>
+                <li><a href="index.php">Sản phẩm</a></li>
+                <li><a href="index.php">Tài khoản</a></li>
+                <li><a href="index.php">Bình luận</a></li>
+                <li><a href="index.php">Thống kê</a></li>
+                <li><a href="../index.php">Trang website</a></li>                
+            </ul>
+        </div>
+    </div>
+
--- model/pdo.php
+++ model/pdo.php
@@ -4,7 +4,7 @@
  */
 function pdo_get_connection()
 {
-    $dburl = "mysql:host=localhost;dbname=duanmau;charset=utf8";
+    $dburl = "mysql:host=localhost;dbname=db_bansach;charset=utf8";
     $username = 'root';
     $password = '';
 
