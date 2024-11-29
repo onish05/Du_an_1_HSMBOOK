845cfae85c5fd2a33ca5c2e6dce5438838d9ed8c
onish05 <phamminh5610@gmail.com>
2024-11-29 07:52:09 +0700

    feat:sanpham

--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-</body>
-</html>
+<?php
+include "../model/pdo.php";
+include "../model/danhmuc.php";
+include "../model/sanpham.php";
+include "header.php";
---
+++ admin/sanpham/add.php
@@ -0,0 +1,50 @@
+<div class="row">
+   <div class="row formtitle">
+      <h2>THÊM SẢN PHẨM</h2>
+   </div>
+   <div class="row formcontent">
+      <form action="index.php?act=addsp" method="post" enctype="multipart/form-data">
+         <div class="row mb10">
+            Danh mục<br />
+            <div class="chondm">
+               <selec name="iddm">
+                  <option value="0">Tất cả</option>
+                  <?php
+                  foreach ($listdanhmuc as $danhmuc) {
+                     extract($danhmuc);
+                     echo '<option value="' . $id . '">' . $name . '</option>';
+                  }
+                  ?>
+               </selec>
+            </div>
+         </div>
+         <div class="row mb10">
+            Tên sản phẩm <br />
+            <input type="text" name="tensp" />
+         </div>
+         <div class="row mb10">
+            Giá <br />
+            <input type="text" name="giasp" />
+         </div>
+         <div class="row mb10">
+            Hình <br />
+            <input type="file" name="hinh">
+         </div>
+         <div class="row mb10">
+            Mô tả <br />
+            <textarea name="mota" rows="2" cols="100"></textarea>
+         </div>
+         <div class="row mb10">
+            <input type="submit" name="themmoi" value="Thêm mới" />
+            <input type="reset" value="Nhập lại" />
+            <a href="index.php?act=listsp"><input type="button" value="Danh sách" /></a>
+            <a href="index.php?act=listsp"><input type="button" value="Danh sách" /></a>
+         </div>
+         <?php
+         if (isset($thongbao) && ($thongbao != "")) {
+            echo $thongbao;
+         }
+         ?>
+      </form>
+   </div>
+</div>
