4efeb45e82c20dbef6c65ddaa9b31f180d99e29f
onish05 <phamminh5610@gmail.com>
2024-12-08 11:09:27 +0700

    fix:boxright

--- view/boxright.php
+++ view/boxright.php
@@ -11,9 +11,11 @@
          <div class="row mb10">
             <li><a href="index.php?act=quenmk">Quên mật khẩu</a></li>
             <li><a href="index.php?act=edit_taikhoan">Cập nhật tài khoản</a></li>
-            <?php if (isset($role) && $role == 1): ?>
-               <li><a href="admin/index.php">Đăng nhập Admin</a></li>
-            <?php endif; ?>
+            <?php
+            if ($role == 1) {
+               ?>
+               <li><a href="admin/index.php">Đăng nhập admin</a></li>
+            <?php } ?>
             <li><a href="index.php?act=thoat">Thoát</a></li>
          </div>
          <?php
@@ -21,61 +23,61 @@
          ?>
          <form action="index.php?act=dangnhap" method="post">
             <div class="row mb10">
-               Tên đăng nhập <br>
-               <input type="text" name="user">
+               Tên đăng nhập <br />
+               <input type="text" name="user" />
             </div>
             <div class="row mb10">
-               Mật khẩu <br>
-               <input type="password" name="pass">
+               Mật khẩu <br />
+               <input type="password" name="pass" />
             </div>
             <div class="row mb10">
-               <input type="checkbox" name="remember"> Ghi nhớ tài khoản?
+               <input type="checkbox" name="remember" />Ghi nhớ tài khoản?
             </div>
             <div class="row mb10">
-               <input type="submit" value="Đăng nhập" name="dangnhap">
+               <input type="submit" value="Đăng nhập" name="dangnhap" />
             </div>
          </form>
-         <li><a href="index.php?act=quenmk">Quên mật khẩu</a></li>
+         <li><a href="#">Quên mật khẩu</a></li>
          <li><a href="index.php?act=dangky">Đăng ký thành viên</a></li>
-         <?php
+      </div>
+   </div>
+   <?php
       }
       ?>
 
-
-      <div class="row mb">
-         <div class="boxtitle">DANH MỤC</div>
-         <div class="boxcontent2">
-            <div class="menudoc listdm">
-               <?php
-               foreach ($dsdm as $dm) {
-                  extract($dm);
-                  $linkdm = "index.php?act=sanpham&iddm" . $id;
-                  echo '<ul><a href="' . $linkdm . '" <li> ' . $name . '</li></a></ul>';
-               }
-               ?>
-            </div>
-            <div class="boxfooter searchbox">
-               <form action="index.php?act=sanpham" method="post">
-                  <input type="text" name="kyw" id="">
-                  <input type="submit" value="Tìm" name="timkiem">
-               </form>
-            </div>
-         </div>
-         <div class="row">
-            <div class="boxtitle">TOP SÁCH YÊU THÍCH</div>
-            <div class="row boxcontent ">
-               <?php
-               foreach ($dstop10 as $sp) {
-                  extract($sp);
-                  $linksp = "index.php?act=sanphamct&idsp=" . $id;
-                  $img = $img_path . $img;
-                  echo '<div class="row mb10 top10">
-                        <a href="' . $linksp . '"><img src="' . $img . '" width="200px" alt="" /></a>
-                        <a href="' . $linksp . '">' . $name . '</a>
-                     </div>';
-               }
-               ?>
-            </div>
-         </div>
+<div class="row mb">
+   <div class="boxtitle">DANH MỤC</div>
+   <div class="boxcontent2">
+      <div class="menudoc listdm">
+         <?php
+         foreach ($dsdm as $dm) {
+            extract($dm);
+            $linkdm = "index.php?act=sanpham&iddm=" . $id;
+            echo '<ul><a href="' . $linkdm . '"><li>' . $name . '</li></a></ul>';
+         }
+         ?>
       </div>
    </div>
+   <div class="boxfooter searchbox">
+      <form action="index.php?act=sanpham" method="post">
+         <input type="text" name="kyw" />
+         <input type="submit" name="timkiem" value="Tìm" />
+      </form>
+   </div>
+</div>
+<div class="row">
+   <div class="boxtitle">TOP SÁCH YÊU THÍCH</div>
+   <div class="row boxcontent">
+      <?php
+      foreach ($dstop10 as $sp) {
+         extract($sp);
+         $linksp = "index.php?act=sanphamct&idsp=" . $id;
+         $img = $img_path . $img;
+         echo '<div class="row mb10 top10">
+               <a href="' . $linksp . '"><img src="' . $img . '" width="300" alt="" /></a>
+               <a href="' . $linksp . '">' . $name . '</a>
+            </div>';
+      }
+      ?>
+   </div>
+</div>
